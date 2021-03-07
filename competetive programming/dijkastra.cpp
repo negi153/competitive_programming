4988bb028b96ce6df23623ade3bdd68adbd8bc37
negi153 <mukesh7758negi@gmail.com>
2021-03-07 15:02:08 +0530

    updating dijkastra.cpp to find the shortest path of every node from source node

--- competetive programming/dijkastra.cpp
+++ competetive programming/dijkastra.cpp
@@ -1,25 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<string.h>//for memset function
-#include<set>//for multiset
-#include<utility>//for pair
+#include<bits/stdc++.h>
 
 using namespace std;
 
 #define SIZE 100000+1
+
 vector<pair<int,int> > v[SIZE];//each vertex has all the connected vertices with the edges weight
 int dis[SIZE];
 bool vis[SIZE];
+int parent[SIZE];
+
 
 void dijkstra(int n){//n is no. of nodes
 
 for(int i=0;i<SIZE;i++)//set all vertices distance as infinity
     dis[i]=2e5;
-dis[1]=0;//set distance of source from surce 0
+
+dis[0]=0;//set distance of source from surce 0
 memset(vis,false,sizeof(vis));//set all vertex as unvisited
+memset(parent,-1,sizeof(parent));//set parent of every node as -1
 
 multiset< pair<int,int> > s;//it is same a min-priority queue
-s.insert({0,1});//insert distance of source as 0 {distance,source node}
+s.insert({0,0});//insert distance of source as 0 {distance,source node}
 
 while(!s.empty()){
 
@@ -27,18 +28,17 @@ while(!s.empty()){
     s.erase(s.begin());
 
     int x=p.second;
-    int w=p.first;
-
-    if(vis[x])
-        continue;//check if popped vertex is visited before
 
     vis[x]=true;
 
-    for(int i=0;i<v[x].size();i++){
+    for(int i=0;i<v[x].size();i++){ //for every adjacent node e we are finding whether there can 
+        // be a shorter path if we go through node x from source s
         int e=v[x][i].first;
         int w=v[x][i].second;
-        if(dis[x]+w<dis[e]){
+
+        if(vis[e]==false and dis[x]+w<dis[e]){
             dis[e]=dis[x]+w;//check if the next vertex distance could be minimized
+            parent[e]=x; //set parent of a node
             s.insert({dis[e],e});//insert the next vertex with the updated distance
         }
     }
@@ -46,26 +46,72 @@ while(!s.empty()){
 }
 
 //showing distance of all nodes from source node 1
-for(int i=2;i<=n;i++)
-    cout<<i<<" "<<dis[i]<<endl;
+cout<<"shortest distance of every node from source node :\n";
 
-
+for(int i=0;i<n;i++)
+    cout<<"0 - "<<i<<" => "<<dis[i]<<endl;
 
 }
 
+
+
+void show_path(int node){
+// this function will show the shortest path of a node  from source 
+    // node and also distance from sourcr
+
+cout<<"distance of node "<<node<<" from source is : "<<dis[node]<<endl;
+
+vector<int> shortest_path;
+
+//from the current node, add the parent to the vector(going in reverse order). then reverse the 
+// vector to find the shortest path
+int temp_node=node;
+
+while(node != -1){
+    
+    shortest_path.push_back(node);
+    node=parent[node];
+
+}
+
+
+cout<<"shortest path of node "<<temp_node<<" from source node : ";
+
+reverse(shortest_path.begin(),shortest_path.end());
+
+for(int i : shortest_path)
+    cout<<i<<"->";
+
+}
+
+
 int main(){
 
+#ifndef ONLINE_JUDGE
+     freopen("input.txt","r",stdin);
+     freopen("output.txt","w",stdout);
+#endif
+
 int s,m,n,from,next,weight;
-cout<<"enter no. of nodes and no. of edges : ";
+// cout<<"enter no. of nodes and no. of edges : ";
 cin>>n>>m;
-cout<<"enter edges from ,next and weight : ";
+// cout<<"enter edges from ,next and weight : ";
+
+bool directed=false;
+
 for(int i=0;i<m;i++){
     cin>>from>>next>>weight;
     v[from].push_back({next,weight});
+
+    if(directed==false)
+        v[next].push_back({from,weight});
 }
 
 dijkstra(n);
 
+//show the shortest path of  node from source node
+
+show_path(4);
 
 return 0;
 }
